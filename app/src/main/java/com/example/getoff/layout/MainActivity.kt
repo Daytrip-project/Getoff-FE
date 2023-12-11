83945b5defa05177c519c169db4ac3ef63828aba
cshiz9206 <cshiz9206@gmail.com>
2023-12-11 15:02:37 +0900

    [fix][feat] start logic
    Resolves
    > location sensor ON request
    > restart after hardware power OFF

--- app/src/main/java/com/example/getoff/layout/MainActivity.kt
+++ app/src/main/java/com/example/getoff/layout/MainActivity.kt
@@ -3,10 +3,19 @@ package com.example.getoff.layout
 import android.Manifest
 import android.annotation.TargetApi
 import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentSender
 import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
@@ -15,13 +24,22 @@ import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import com.example.getoff.R
+import com.example.getoff.util.GpsUtil
 import com.example.getoff.worker.SensorWorker
+import com.example.getoff.worker.SensorWorker.Companion.scheduleSensorWorker
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
 import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
 
-    private val REQUEST_LOCATION = 1
+    companion object {
+        private const val REQUEST_CHECK_SETTINGS = 1001
+        private const val REQUEST_LOCATION = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +47,47 @@ class MainActivity : AppCompatActivity() {
 
         if (requestLocationPermission()) {
             proceedAfterPermission()
+            if (!isLocationEnabled()) {
+                infoSetLocation()
+            }
         }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+
+        if (hasFocus) {
+            if (!isLocationEnabled()) {
+                infoSetLocation()
+            }
+        }
+    }
+
+//    override fun onResume() {
+//        super.onResume()
+//
+//            if (!isLocationEnabled()) {
+//                finishAffinity()
+//            }
+//        }
+//    }
+
+    private fun infoSetLocation() {
+        AlertDialog.Builder(this)
+            .setTitle("위치 서비스 필요")
+            .setMessage("이 앱은 위치 서비스가 필요합니다. 설정에서 위치 서비스를 활성화해주세요.")
+            .setPositiveButton("설정으로 이동") { dialog, which ->
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+            .setNegativeButton("취소", null)
+            .show()
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) &&
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
@@ -48,7 +106,7 @@ class MainActivity : AppCompatActivity() {
         // 첫 실행 확인
 //        val prefs = getSharedPreferences("daytrip_getoff_app_local_context_registry", Context.MODE_PRIVATE)
 //        if (!prefs.getBoolean("app_execute", false)) {
-            scheduleSensorWorker()
+            scheduleSensorWorker(this)
 //            prefs.edit().putBoolean("app_execute", true).apply()
 //        }
 
@@ -56,18 +114,6 @@ class MainActivity : AppCompatActivity() {
         val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.mainContainer, newFragment)
         transaction.commit()
-    }
-
-    private fun scheduleSensorWorker() {
-        val workRequest = PeriodicWorkRequestBuilder<SensorWorker>(1, TimeUnit.MINUTES)
-            .build()
-
-//        WorkManager.getInstance(this).enqueue(workRequest)
-        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
-            "sensor_worker",
-            ExistingPeriodicWorkPolicy.KEEP,
-            workRequest
-        )
     }
 
     /** 위치 권한 SDK 버전 29 이상**/
